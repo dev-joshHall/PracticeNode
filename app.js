484d33c2f9bdfa4c3febbcfda78b15e1213ce41d
dev-joshHall <63134862+dev-joshHall@users.noreply.github.com>
2022-05-12 10:50:32 -0600

    first node commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const {readFile, writeFile} = require("fs");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+    if (err) {
+        return;
+    }
+    console.log(result);
+})
---
+++ names.js
@@ -0,0 +1,5 @@
+let john = "John";
+let alice = "Alice";
+let bryce = "Bryce";
+
+module.exports = {john, alice, bryce};
