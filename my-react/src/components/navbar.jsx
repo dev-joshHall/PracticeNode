127105bd5453d9a36bf913d0f9d907d85b985d53
dev-joshHall <63134862+dev-joshHall@users.noreply.github.com>
2022-06-03 18:28:25 -0600

    added a test message

--- my-react/src/components/navbar.jsx
+++ my-react/src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const NavBar = (props) => {
+    console.log("hello")
     return (
         <nav className="navbar bg-light">
             <div className="container-fluid">
